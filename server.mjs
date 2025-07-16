dd78e7038bf8d8e03c341c26a755b6db3db582b4
quaziyadgar <quaziyadgar@gmail.com>
2025-07-16 15:40:29 +0530

    chenges in api route

--- server.mjs
+++ server.mjs
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/leaderboard', leaderboardRoutes);
+app.use('/api', leaderboardRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
