1d9b03d1055e5c9b5f5a71dfb72ae7268c424690
quaziyadgar <quaziyadgar@gmail.com>
2025-07-16 16:07:27 +0530

    some fixes

--- config/db.mjs
+++ config/db.mjs
@@ -3,7 +3,11 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', true);
-        await mongoose.connect(process.env.MONGODB_URI, {
+        const MONGODB_URI = process.env.MONGODB_URI;
+        if (!MONGODB_URI) {
+            throw new Error('MONGODB_URI is not defined in environment variables');
+          }
+        await mongoose.connect(MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
--- server.mjs
+++ server.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import connectDB from './config/db.mjs';
 import leaderboardRoutes from './routes/leaderboard.mjs';
 import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
 
